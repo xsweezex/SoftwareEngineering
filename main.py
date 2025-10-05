ca609eabf817d9035ebf1b35f557109b560afb59
xsweezex <101130601+xsweezex@users.noreply.github.com>
2025-10-05 20:17:44 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from wasd import qwerty
+
+if __name__ == '__main__':
+    a = int(input("a: "))
+    b = int(input("b: "))
+    c = int(input("c: "))
+    print("Площадь треугольника:", qwerty(a, b, c))
